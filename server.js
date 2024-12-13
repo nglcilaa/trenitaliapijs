d7332e34d60d5952a5f8049d28d5678db7ea78ba
nglcilaa <info@nglcila.com>
2024-12-13 18:19:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import express from 'express';
 import cors from 'cors';
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -38,6 +37,8 @@ const getDepartures = async (partenzaId, datetime) => {
   return get('partenze', partenzaId, datetime);
 };
 
+
+
 const stazioniGenova = [
   "GENOVA BRIGNOLE",
   "GENOVA PIAZZA PRINCIPE",
@@ -50,23 +51,50 @@ const stazioniGenova = [
   "GENOVA SAMPIERDARENA"
 ];
 
-const getTrainIcon = (categoria, regione, destinazione) => {
+const stazioniSpeciali = [
+  "ASTI", "RIMINI", "RAVENNA", "PADOVA", "VENEZIA", 
+   "MODENA", "LIVORNO", "PISA"
+];
+
+const stazioniLombardia = [
+  "BRESCIA", "PAVIA", "PIOLTELLO LIMITO", "VARESE", 
+  "MORTARA", "TREVIGLIO", "MILANO GRECO PIRELLI", "MI.P.GENOVA"
+];
+
+const getTrainIcon = (categoria, regione, destinazione, partenza) => {
   if (categoria === "REG") {
     if (regione === 1) { // 1 corrisponde a LOMBARDIA
-      if (destinazione.includes("BOLOGNA") || destinazione.includes("TORINO") || stazioniGenova.some(staz => destinazione.includes(staz))) {
+      if (
+        destinazione.includes("BOLOGNA") ||
+        destinazione.includes("TORINO") ||
+        stazioniGenova.some(staz => destinazione.includes(staz)) ||
+        stazioniSpeciali.some(staz => destinazione.includes(staz))
+      ) {
         return "https://seeklogo.com/images/T/trenitalia-logo-0AE98832B5-seeklogo.com.png";
+      } else if (
+        partenza === "VERONA PORTA NUOVA" && destinazione === "MILANO CENTRALE" || 
+        partenza === "MILANO CENTRALE" && destinazione === "VERONA PORTA NUOVA"
+      ) {
+        return "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Trenord_Logo.svg/800px-Trenord_Logo.svg.png";
       } else {
         return "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Trenord_Logo.svg/800px-Trenord_Logo.svg.png";
       }
+    } else if (stazioniLombardia.some(staz => destinazione.includes(staz))) {
+      return "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Trenord_Logo.svg/800px-Trenord_Logo.svg.png";
     } else {
       return "https://seeklogo.com/images/T/trenitalia-logo-0AE98832B5-seeklogo.com.png";
     }
-  } else if (categoria === "FR") {
-    return "https://upload.wikimedia.org/wikipedia/commons/8/81/Frecciarossa_TR_-_Logo.svg";
+  } else if (categoria === "IC") {
+    return "https://upload.wikimedia.org/wikipedia/commons/8/86/Treno_Intercity.svg";
+  } else if (categoria === "EC") {
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/SBB_EC_Logo.svg/2560px-SBB_EC_Logo.svg.png";
   } else {
     return "https://upload.wikimedia.org/wikipedia/commons/8/81/Frecciarossa_TR_-_Logo.svg";
   }
 };
+
+
+
 
 app.get('/stations/:partenza_id/departures', async (req, res) => {
   const { partenza_id } = req.params;
