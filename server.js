576b2db9fa44d5ed8a30d6193a1314f25d5a5ba6
nglcilaa <info@nglcila.com>
2024-12-19 22:04:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 import { getRegion, getDepartures, getTrainIcon, stazioniGenova, stazioniSpeciali, stazioniLombardia } from './functions/trainService.js';
 import { fetchNewsFromTicker, fetchNewsFromRSS } from './functions/fetchNewsRFI.js';
 import { getTrainDetails } from './functions/fetchDetailTrains.js';
@@ -10,6 +11,25 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const getTrainOrigin = async (numeroTreno) => {
+  const url = `http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/cercaNumeroTreno/${numeroTreno}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data; // Ottieni la risposta JSON
+
+    // Estrarre la stazione di provenienza dal campo descLocOrig
+    if (data && data.descLocOrig) {
+      return data.descLocOrig;
+    } else {
+      throw new Error('Stazione di provenienza non trovata');
+    }
+  } catch (error) {
+    console.error('Errore nella chiamata API:', error);
+    throw new Error('Errore nel recupero delle informazioni sul treno');
+  }
+};
 
 app.get('/stations/:partenza_id/departures', async (req, res) => {
   const { partenza_id } = req.params;
@@ -25,76 +45,82 @@ app.get('/stations/:partenza_id/departures', async (req, res) => {
     const departures = await getDepartures(partenza_id, datetime);
     console.log('Departures:', departures);
 
-    const result = departures.map(d => ({
-      numeroTreno: parseInt(d.numeroTreno),
-      categoria: d.categoria,
-      categoriaDescrizione: d.categoriaDescrizione.trim(),
-      origine: d.origine,
-      codOrigine: d.codOrigine,
-      destinazione: d.destinazione,
-      codDestinazione: d.codDestinazione,
-      oraPartenzaEstera: d.oraPartenzaEstera,
-      oraArrivoEstera: d.oraArrivoEstera,
-      tratta: d.tratta,
-      regione: d.regione,
-      origineZero: d.origineZero,
-      destinazioneZero: d.destinazioneZero,
-      orarioPartenzaZero: d.orarioPartenzaZero,
-      orarioArrivoZero: d.orarioArrivoZero,
-      circolante: d.circolante,
-      codiceCliente: d.codiceCliente,
-      binarioEffettivoArrivoCodice: d.binarioEffettivoArrivoCodice,
-      binarioEffettivoArrivoDescrizione: d.binarioEffettivoArrivoDescrizione,
-      binarioEffettivoArrivoTipo: d.binarioEffettivoArrivoTipo,
-      binarioProgrammatoArrivoCodice: d.binarioProgrammatoArrivoCodice,
-      binarioProgrammatoArrivoDescrizione: d.binarioProgrammatoArrivoDescrizione,
-      binarioEffettivoPartenzaCodice: d.binarioEffettivoPartenzaCodice,
-      binarioEffettivoPartenzaDescrizione: d.binarioEffettivoPartenzaDescrizione,
-      binarioEffettivoPartenzaTipo: d.binarioEffettivoPartenzaTipo,
-      binarioProgrammatoPartenzaCodice: d.binarioProgrammatoPartenzaCodice,
-      binarioProgrammatoPartenzaDescrizione: d.binarioProgrammatoPartenzaDescrizione,
-      subTitle: d.subTitle,
-      esisteCorsaZero: d.esisteCorsaZero,
-      orientamento: d.orientamento,
-      inStazione: d.inStazione,
-      haCambiNumero: d.haCambiNumero,
-      nonPartito: d.nonPartito,
-      provvedimento: d.provvedimento,
-      riprogrammazione: d.riprogrammazione,
-      orarioPartenza: new Date(d.orarioPartenza).getTime(),
-      orarioArrivo: d.orarioArrivo ? new Date(d.orarioArrivo).getTime() : null,
-      stazionePartenza: d.stazionePartenza,
-      stazioneArrivo: d.stazioneArrivo,
-      statoTreno: d.statoTreno,
-      corrispondenze: d.corrispondenze,
-      servizi: d.servizi,
-      ritardo: d.ritardo,
-      tipoProdotto: d.tipoProdotto,
-      compOrarioPartenzaZeroEffettivo: d.compOrarioPartenzaZeroEffettivo,
-      compOrarioArrivoZeroEffettivo: d.compOrarioArrivoZeroEffettivo,
-      compOrarioPartenzaZero: d.compOrarioPartenzaZero,
-      compOrarioArrivoZero: d.compOrarioArrivoZero,
-      compOrarioArrivo: d.compOrarioArrivo,
-      compOrarioPartenza: d.compOrarioPartenza,
-      compNumeroTreno: d.compNumeroTreno,
-      compOrientamento: d.compOrientamento,
-      compTipologiaTreno: d.compTipologiaTreno,
-      compClassRitardoTxt: d.compClassRitardoTxt,
-      compClassRitardoLine: d.compClassRitardoLine,
-      compImgRitardo2: d.compImgRitardo2,
-      compImgRitardo: d.compImgRitardo,
-      compRitardo: d.compRitardo,
-      compRitardoAndamento: d.compRitardoAndamento,
-      compInStazionePartenza: d.compInStazionePartenza,
-      compInStazioneArrivo: d.compInStazioneArrivo,
-      compOrarioEffettivoArrivo: d.compOrarioEffettivoArrivo,
-      compDurata: d.compDurata,
-      compImgCambiNumerazione: d.compImgCambiNumerazione,
-      materiale_label: d.materiale_label,
-      dataPartenzaTreno: new Date(d.dataPartenzaTreno).getTime(),
-      binarioProgrammato: d.binarioProgrammatoPartenzaDescrizione,
-      binarioReale: d.binarioEffettivoPartenzaDescrizione,
-      iconaTreno: getTrainIcon(d.categoria, regione, d.destinazione, d.partenza, d.codiceCliente)
+    const result = await Promise.all(departures.map(async d => {
+      const origineStazione = await getTrainOrigin(d.numeroTreno); // Ottieni stazione di provenienza
+
+      return {
+        numeroTreno: parseInt(d.numeroTreno),
+        categoria: d.categoria,
+        categoriaDescrizione: d.categoriaDescrizione.trim(),
+        origine: d.origine,
+        stazioneDiProvenienza: origineStazione,
+        codOrigine: d.codOrigine,
+        destinazione: d.destinazione,
+        codDestinazione: d.codDestinazione,
+        oraPartenzaEstera: d.oraPartenzaEstera,
+        oraArrivoEstera: d.oraArrivoEstera,
+        tratta: d.tratta,
+        regione: d.regione,
+        origineZero: d.origineZero,
+        destinazioneZero: d.destinazioneZero,
+        orarioPartenzaZero: d.orarioPartenzaZero,
+        orarioArrivoZero: d.orarioArrivoZero,
+        circolante: d.circolante,
+        codiceCliente: d.codiceCliente,
+        binarioEffettivoArrivoCodice: d.binarioEffettivoArrivoCodice,
+        binarioEffettivoArrivoDescrizione: d.binarioEffettivoArrivoDescrizione,
+        binarioEffettivoArrivoTipo: d.binarioEffettivoArrivoTipo,
+        binarioProgrammatoArrivoCodice: d.binarioProgrammatoArrivoCodice,
+        binarioProgrammatoArrivoDescrizione: d.binarioProgrammatoArrivoDescrizione,
+        binarioEffettivoPartenzaCodice: d.binarioEffettivoPartenzaCodice,
+        binarioEffettivoPartenzaDescrizione: d.binarioEffettivoPartenzaDescrizione,
+        binarioEffettivoPartenzaTipo: d.binarioEffettivoPartenzaTipo,
+        binarioProgrammatoPartenzaCodice: d.binarioProgrammatoPartenzaCodice,
+        binarioProgrammatoPartenzaDescrizione: d.binarioProgrammatoPartenzaDescrizione,
+        subTitle: d.subTitle,
+        esisteCorsaZero: d.esisteCorsaZero,
+        orientamento: d.orientamento,
+        inStazione: d.inStazione,
+        haCambiNumero: d.haCambiNumero,
+        nonPartito: d.nonPartito,
+        provvedimento: d.provvedimento,
+        riprogrammazione: d.riprogrammazione,
+        orarioPartenza: new Date(d.orarioPartenza).getTime(),
+        orarioArrivo: d.orarioArrivo ? new Date(d.orarioArrivo).getTime() : null,
+        stazionePartenza: d.stazionePartenza,
+        stazioneArrivo: d.stazioneArrivo,
+        statoTreno: d.statoTreno,
+        corrispondenze: d.corrispondenze,
+        servizi: d.servizi,
+        ritardo: d.ritardo,
+        tipoProdotto: d.tipoProdotto,
+        compOrarioPartenzaZeroEffettivo: d.compOrarioPartenzaZeroEffettivo,
+        compOrarioArrivoZeroEffettivo: d.compOrarioArrivoZeroEffettivo,
+        compOrarioPartenzaZero: d.compOrarioPartenzaZero,
+        compOrarioArrivoZero: d.compOrarioArrivoZero,
+        compOrarioArrivo: d.compOrarioArrivo,
+        compOrarioPartenza: d.compOrarioPartenza,
+        compNumeroTreno: d.compNumeroTreno,
+        compOrientamento: d.compOrientamento,
+        compTipologiaTreno: d.compTipologiaTreno,
+        compClassRitardoTxt: d.compClassRitardoTxt,
+        compClassRitardoLine: d.compClassRitardoLine,
+        compImgRitardo2: d.compImgRitardo2,
+        compImgRitardo: d.compImgRitardo,
+        compRitardo: d.compRitardo,
+        compRitardoAndamento: d.compRitardoAndamento,
+        compInStazionePartenza: d.compInStazionePartenza,
+        compInStazioneArrivo: d.compInStazioneArrivo,
+        compOrarioEffettivoArrivo: d.compOrarioEffettivoArrivo,
+        compDurata: d.compDurata,
+        compImgCambiNumerazione: d.compImgCambiNumerazione,
+        materiale_label: d.materiale_label,
+        dataPartenzaTreno: new Date(d.dataPartenzaTreno).getTime(),
+        binarioProgrammato: d.binarioProgrammatoPartenzaDescrizione,
+        binarioReale: d.binarioEffettivoPartenzaDescrizione,
+        iconaTreno: getTrainIcon(d.categoria, regione, d.destinazione, d.partenza, d.codiceCliente),
+        stazioneDiProvenienza: origineStazione // Nuovo campo aggiunto
+      };
     }));
 
     res.json(result);
@@ -103,6 +129,7 @@ app.get('/stations/:partenza_id/departures', async (req, res) => {
     res.status(500).json({ error: 'Errore nel recupero delle partenze.' });
   }
 });
+
 
 app.get('/news/ticker', async (req, res) => {
   try {
@@ -150,6 +177,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+export default { getTrainOrigin }
 
 
 
